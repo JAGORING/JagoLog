cc9d3d72828a98c603de101dae8d81ee273d8c10
JAGORING <lovehaha11@gmail.com>
2025-01-19 12:59:54 +0900

    [Style] layout scroll 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,14 +16,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className="bg-cover bg-center h-full min-h-screen flex items-center justify-center px-2 xl:px-0"
+        className="bg-cover bg-center h-screen flex flex-col items-center justify-center px-4 xl:px-0"
         style={{ backgroundImage: "url('/images/body_bg.jpg')" }}
       >
-        <div className="p-2 bg-black/20 backdrop-blur-md shadow-lg rounded-xl w-full max-w-[1200px] h-[80vh] lg:h-[75vh] mx-auto my-6 overflow-y-auto scrollbar-thin scrollbar-thumb-rounded">
+        <div className="p-4 bg-black/20 backdrop-blur-md shadow-lg rounded-xl w-full max-w-5xl h-[80vh] lg:h-[75vh]">
           <Header />
-          <main className="min-h-[67vh] lg:min-h-[62vh] flex flex-col">{children}</main>
-          <Footer />
+          <main className="h-[calc(75vh-60px)] lg:h-[calc(70vh-60px)] flex flex-col overflow-y-auto scrollbar-thin scrollbar-thumb-rounded">
+            {children}
+          </main>
         </div>
+        <Footer />
       </body>
     </html>
   );
