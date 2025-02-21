aed48501ddb985e62a8b2b2607974fa4e6285f54
JAGORING <lovehaha11@gmail.com>
2025-02-21 14:39:49 +0900

    [style] main layout ui 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,14 +16,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className="bg-cover bg-center h-screen flex flex-col items-center justify-center px-4 xl:px-0"
+        className="bg-cover bg-center h-screen flex flex-col items-center justify-between px-4 xl:px-0"
         style={{ backgroundImage: "url('/images/body_bg.jpg')" }}
       >
-        <div className="p-4 bg-black/35 backdrop-blur-md shadow-lg rounded-xl w-full max-w-5xl h-[80vh] lg:h-[75vh]">
-          <Header />
-          <main className="px-3 h-[calc(75vh-60px)] lg:h-[calc(70vh-60px)] flex flex-col overflow-y-auto scrollbar-thin scrollbar-thumb-rounded">
-            {children}
-          </main>
+        <div className="h-full w-full flex items-center justify-center">
+          <div className="p-4 bg-black/35 backdrop-blur-md shadow-lg rounded-xl w-full max-w-5xl h-[80vh] lg:h-[75vh]">
+            <Header />
+            <main className="px-3 h-[calc(75vh-60px)] lg:h-[calc(70vh-60px)] flex flex-col overflow-y-auto scrollbar-thin scrollbar-thumb-rounded">
+              {children}
+            </main>
+          </div>
         </div>
         <Footer />
       </body>
--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="absolute bottom-2 h-[50px] flex justify-center items-center text-gray-200 text-sm">
+    <footer className="bottom-2 h-[50px] flex justify-center items-center text-gray-200 text-sm">
       © 2025 JAGO all rights reserved
     </footer>
   );
