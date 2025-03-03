4b62ee701e5ecc223e39245451bc5d908869f9f9
JAGORING <lovehaha11@gmail.com>
2025-03-03 17:26:43 +0900

    [Feat] postCard title, description 긴 경우 줄임표 적용 및 mdx 파일 내용 수정

--- src/components/post-list/PostCard.tsx
+++ src/components/post-list/PostCard.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export default function PostCard(props: Post) {
   return (
-    <Link href={props.url} className="block">
-      <div className="relative rounded-2xl border border-white/10 bg-white/5 shadow-md transition-all duration-300 hover:border-white/50 hover:shadow-lg">
+    <Link href={props.url} className="block h-full">
+      <div className="relative h-full flex flex-col rounded-2xl border border-white/10 bg-white/5 shadow-md transition-all duration-300 hover:border-white/50 hover:shadow-lg">
         {props.thumbnail && (
           <div className="w-full h-48 rounded-t-xl overflow-hidden">
             <Image
@@ -16,10 +16,10 @@ export default function PostCard(props: Post) {
             />
           </div>
         )}
-        <div className="p-3">
+        <div className="p-3 flex flex-col flex-grow">
           <div className="text-gray-300 text-sm">{props.date}</div>
-          <p className="text-lg font-semibold text-white mt-1">{props.title}</p>
-          <p className="text-xs text-gray-200 mt-1">{props.description}</p>
+          <p className="text-lg font-semibold text-white mt-1 line-clamp-1">{props.title}</p>
+          <p className="text-xs text-gray-200 mt-1 line-clamp-2 flex-grow">{props.description}</p>
         </div>
       </div>
     </Link>
