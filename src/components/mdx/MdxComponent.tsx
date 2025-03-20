0c807a7f5c93b841ee26a04969d21a49df2127cc
JAGORING <lovehaha11@gmail.com>
2025-03-21 00:30:24 +0900

    [Feat] 새로운 게시글 작성 : JavaScript 스터디

--- src/components/mdx/MdxComponent.tsx
+++ src/components/mdx/MdxComponent.tsx
@@ -6,7 +6,7 @@ import { Image } from './Image';
 export const mdxComponents: MDXComponents = {
   a: ({ children, ...props }) => {
     return (
-      <Link {...props} href={props.href || ''}>
+      <Link {...props} href={props.href || ''} className="underline underline-offset-4 text-pointColor">
         {children}
       </Link>
     );
@@ -18,7 +18,7 @@ export const mdxComponents: MDXComponents = {
   h1: (props) => <h1 className="text-2xl font-bold my-6" {...props} />,
   h2: (props) => <h2 className="text-xl font-semibold my-5" {...props} />,
   h3: (props) => <h3 className="text-lg font-semibold my-5" {...props} />,
-  p: (props) => <p className="text-sm my-3" {...props} />,
-  ul: (props) => <ul className="text-sm list-disc pl-5 my-4" {...props} />,
+  p: (props) => <p className="text-md my-4" {...props} />,
+  ul: (props) => <ul className="text-md list-disc pl-5 my-4" {...props} />,
   li: (props) => <li className="mb-2" {...props} />,
 };
