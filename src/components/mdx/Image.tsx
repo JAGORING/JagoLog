763b012222cd862f28da6cb28d42756dd4f52fc1
JAGORING <lovehaha11@gmail.com>
2025-05-21 17:34:26 +0900

    [Feat] 새로운 게시글 작성 :  2021~2022 사이드 프로젝트

--- src/components/mdx/Image.tsx
+++ src/components/mdx/Image.tsx
@@ -11,7 +11,7 @@ export const ImageCom = ({ src, alt }: ImageProps) => {
         <div className="relative aspect-video w-full overflow-hidden rounded-md">
           <Image src={src} alt={alt} fill className="object-contain" sizes="(max-width: 768px) 100vw, 500px" />
         </div>
-        {alt && <span className="block text-center text-sm text-accent-foreground">{alt}</span>}
+        {alt && <span className="mt-3 block text-center text-sm text-accent-foreground">{alt}</span>}
       </div>
     </>
   );
